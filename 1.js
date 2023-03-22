bc07031ff987fcc48b21abc4c5b082baf214e584
azizovvs05 <yasar.azizli010@gmail.com>
2023-03-22 06:04:48 -0700

    pajar

---
+++ 1.js
@@ -0,0 +1,62 @@
+ // Arithmetic
+var a = 729 + (-5126) + (-629) + 5128;
+ console.log(a);
+ // Arithmetic
+var a= (-638) + (-173) + 538 + 173;
+console.log(a);
+
+ // Arithmetic
+var a= (-372) + 984 + 172 + (-2084);
+console.log(a);
+
+ // Arithmetic
+var a= (-78) + 36 + 19 + (-22) + (-25);
+console.log(a);
+
+ // Arithmetic
+var a= 125 / (-25) + 100 * (-2);
+console.log(a);
+
+ // Arithmetic
+var a= 745 / (-5) - 42 / (-21);
+console.log(a);
+
+ // Arithmetic
+var a= 90 * (-3) / 27 + 12;
+console.log(a);
+
+ // Arithmetic
+var a= (-1200) / (-60) - 64 / (-8);
+console.log(a);
+
+ // Arithmetic
+ var b=3;
+ var a= 2 + (b-4);
+ console.log(a);
+
+  // Arithmetic
+  var y=4;
+  var a= -(3-y) + 7;
+  console.log(a);
+
+   // Arithmetic
+   var x=3;
+   var y=4;
+   var a= 2 * (x+y) - 2 * x;
+   console.log(a);
+
+    // Arithmetic
+    var y=5;
+    var a= (3+y) - 42;
+    console.log(a);
+
+     // Arithmetic
+     var x=2;
+     var a=(x-5) + (x+7);
+     console.log(a);
+
+      // Arithmetic
+      var a=2;
+      var b=5;
+      var c= 4 * (a - 2 * b) + 8 * b;
+      console.log(c);
